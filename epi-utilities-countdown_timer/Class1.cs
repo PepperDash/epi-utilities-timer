ab642f8d0c0f1ad3772f2eeb1ddb9c3ee9c07945
Nick Genovese <ngenovese@pepperdash.com>
2020-03-03 13:50:49 -0500

    initia repo seed

---
+++ epi-utilities-countdown_timer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
+
+namespace epi_utilities_countdown_timer
+{
+    public class Class1
+    {
+    }
+}
+
---
+++ epi-utilities-countdown_timer/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("epi_utilities_countdown_timer")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("epi_utilities_countdown_timer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
