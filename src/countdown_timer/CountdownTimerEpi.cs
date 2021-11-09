045d41c24a69860f7c671958c97a6ed1b2b95095
Trevor Payne <tpayne@pepperdash.com>
2021-11-09 16:06:22 -0600

    feature:Change device to Reconfigurable

--- src/countdown_timer/CountdownTimerEpi.cs
+++ src/countdown_timer/CountdownTimerEpi.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Linq;
 using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
-using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
 using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Bridges;
 using PepperDash.Essentials.Core.Config;
 using PepperDash.Core;
-using PepperDash.Essentials.Bridges;
-using Crestron.SimplSharp.Reflection;
+using PepperDash.Essentials.Core.Devices;
 using Newtonsoft.Json;
 
 namespace epi_utilities_countdown_timer
 {
-    public class CountdownTimerEpi : Device, IBridge
+    public class CountdownTimerEpi : ReconfigurableDevice, IBridgeAdvanced
     {
         private readonly SecondsCountdownTimer _countdownTimer;
 
@@ -26,6 +24,10 @@ namespace epi_utilities_countdown_timer
             }
         }
 
+        private string Key;
+
+        private DeviceConfig _Dc;
+
         private int _secondsToCount;
         private readonly int? _warningTime;
         private readonly int? _extendTime;
@@ -35,6 +37,7 @@ namespace epi_utilities_countdown_timer
         public BoolFeedbackPulse TimerWarningFb { get; private set; }
 
         public IntFeedback TimerPercentageFb { get { return _countdownTimer.PercentFeedback; } }
+        public IntFeedback TimerSetFeedback { get; private set; }
 
         public StringFeedback TimerValueFb { get { return _countdownTimer.TimeRemainingFeedback; } }
 
@@ -47,13 +50,15 @@ namespace epi_utilities_countdown_timer
                     timer.SecondsToCount = props.CountdownTime;
 
                     return 
-                        new CountdownTimerEpi(config.Key, timer, props);
+                        new CountdownTimerEpi(config, timer, props);
                 });
         }
 
-        public CountdownTimerEpi(string key, SecondsCountdownTimer timer, CountdowmTimerPropsConfig props)
-            : base(key)
+        public CountdownTimerEpi(DeviceConfig config, SecondsCountdownTimer timer, CountdowmTimerPropsConfig props)
+            : base(config)
         {
+            _Dc = config;
+            Key = config.Key;
             _secondsToCount = props.CountdownTime;
             _countdownTimer = timer;
             _warningTime = props.WarningTime;
@@ -73,6 +78,7 @@ namespace epi_utilities_countdown_timer
 
         public override bool CustomActivate()
         {
+            TimerSetFeedback = new IntFeedback(() => _secondsToCount);
             Debug.Console(2, _countdownTimer, "Activated a new timer with a {0} second countdown", _countdownTimer.SecondsToCount);
 
             _countdownTimer.HasStarted += (sender, args) =>
@@ -135,16 +141,57 @@ namespace epi_utilities_countdown_timer
             _countdownTimer.SecondsToCount = timeToExtend;
             _countdownTimer.Reset();
         }
-
-        #region IBridge Members
-
-        public void LinkToApi(Crestron.SimplSharpPro.DeviceSupport.BasicTriList trilist, uint joinStart, string joinMapKey)
+        public void SetSecondsToCount(ushort value)
         {
-            this.LinkToApiExt(trilist, joinStart, joinMapKey);
+            Timer.SecondsToCount = value;
+            _Dc.Properties["countdownTime"] = value;
+            CustomSetConfig(_Dc);
+            TimerSetFeedback.FireUpdate();
+
         }
 
-        #endregion
+        public void LinkToApi(Crestron.SimplSharpPro.DeviceSupport.BasicTriList trilist, uint joinstart, string joinMapKey, EiscApiAdvanced bridge)
+        {
+            var joinMap = new CountdownTimerJoinMapAdvanced(joinstart);
+
+            if(bridge != null) bridge.AddJoinMap(String.Format(this.Key), joinMap);
+
+            TimerRunningFb.LinkInputSig(trilist.BooleanInput[joinMap.TimerCounting.JoinNumber]);
+            TimerExpiredFb.Feedback.LinkInputSig(trilist.BooleanInput[joinMap.TimerExpired.JoinNumber]);
+            TimerWarningFb.Feedback.LinkInputSig(trilist.BooleanInput[joinMap.TimerWarning.JoinNumber]);
+
+            TimerPercentageFb.LinkInputSig(trilist.UShortInput[joinMap.TimerPercentage.JoinNumber]);
+            TimerValueFb.LinkInputSig(trilist.StringInput[joinMap.TimerValue.JoinNumber]);
+
+            TimerSetFeedback.LinkInputSig(trilist.UShortInput[joinMap.TimerCountdownSetFb.JoinNumber]);
+
+            trilist.SetSigTrueAction(joinMap.TimerStart.JoinNumber, () =>
+                {
+                    Timer.Start();
+                });
+
+            trilist.SetSigTrueAction(joinMap.TimerCancel.JoinNumber, () =>
+                {
+                    Timer.Cancel();
+                });
+
+            trilist.SetSigTrueAction(joinMap.TimerFinish.JoinNumber, () =>
+                {
+                    Timer.Finish();
+                });
+
+            trilist.SetUShortSigAction(joinMap.TimerCountdownSet.JoinNumber, value =>
+                {
+                    SecondsToCount = value;
+                });
+
+            trilist.SetSigTrueAction(joinMap.TimerExtend.JoinNumber, Extend);
+        }
+
+
     }
+
+    
 
     public class CountdowmTimerPropsConfig
     {
@@ -153,95 +200,121 @@ namespace epi_utilities_countdown_timer
         public int? ExtendTime { get; set; }
     }
 
-    public static class CountdownTimerBridge
+
+    public class CountdownTimerJoinMapAdvanced : JoinMapBaseAdvanced
     {
-        public static void LinkToApiExt(this CountdownTimerEpi timer, Crestron.SimplSharpPro.DeviceSupport.BasicTriList trilist, uint joinStart, string joinMapKey)
+        [JoinName("TimerStart")]
+        public JoinDataComplete TimerStart =
+            new JoinDataComplete(new JoinData { JoinNumber = 1, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Start Timer",
+                JoinCapabilities = eJoinCapabilities.FromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+
+        [JoinName("TimerCancel")]
+        public JoinDataComplete TimerCancel =
+            new JoinDataComplete(new JoinData { JoinNumber = 2, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Cancel Timer",
+                JoinCapabilities = eJoinCapabilities.FromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+        [JoinName("TimerFinish")]
+        public JoinDataComplete TimerFinish =
+            new JoinDataComplete(new JoinData { JoinNumber = 3, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Finish Timer",
+                JoinCapabilities = eJoinCapabilities.FromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+        [JoinName("TimerExtend")]
+        public JoinDataComplete TimerExtend =
+            new JoinDataComplete(new JoinData { JoinNumber = 1, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Start Timer",
+                JoinCapabilities = eJoinCapabilities.FromSIMPL,
+                JoinType = eJoinType.Digital
+            });
+        [JoinName("TimerCounting")]
+        public JoinDataComplete TimerCounting =
+            new JoinDataComplete(new JoinData { JoinNumber = 1, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Timer Is Counting",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Digital
+            });
+        [JoinName("TimerExpired")]
+        public JoinDataComplete TimerExpired =
+            new JoinDataComplete(new JoinData { JoinNumber = 2, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Timer Is Expired",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Digital
+            });
+        [JoinName("TimerWarning")]
+        public JoinDataComplete TimerWarning =
+            new JoinDataComplete(new JoinData { JoinNumber = 3, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Timer is in Warning Mode",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Digital
+            });
+
+        [JoinName("TimerCountdownSet")]
+        public JoinDataComplete TimerCountdownSet =
+            new JoinDataComplete(new JoinData { JoinNumber = 1, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Set Timer Value",
+                JoinCapabilities = eJoinCapabilities.FromSIMPL,
+                JoinType = eJoinType.Analog
+            });
+
+        [JoinName("TimerCountdownSetFb")]
+        public JoinDataComplete TimerCountdownSetFb =
+            new JoinDataComplete(new JoinData { JoinNumber = 2, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Get Timer Value",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Analog
+            });
+
+
+        [JoinName("TimerPercentage")]
+        public JoinDataComplete TimerPercentage =
+            new JoinDataComplete(new JoinData { JoinNumber = 1, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Timer Percentage Remaining",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Analog
+            });
+
+        [JoinName("TimerValue")]
+        public JoinDataComplete TimerValue =
+            new JoinDataComplete(new JoinData { JoinNumber = 1, JoinSpan = 1 },
+            new JoinMetadata
+            {
+                Description = "Timer Text Remaining",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Serial
+            });
+
+        public CountdownTimerJoinMapAdvanced(uint joinStart)
+            : base(joinStart, typeof(CountdownTimerJoinMapAdvanced))
         {
-            var joinMap = new CountdownTimerJoinMap(joinStart);
-
-            timer.TimerRunningFb.LinkInputSig(trilist.BooleanInput[joinMap.TimerCounting]);
-            timer.TimerExpiredFb.Feedback.LinkInputSig(trilist.BooleanInput[joinMap.TimerExpired]);
-            timer.TimerWarningFb.Feedback.LinkInputSig(trilist.BooleanInput[joinMap.TimerWarning]);
-
-            timer.TimerPercentageFb.LinkInputSig(trilist.UShortInput[joinMap.TimerPercentage]);
-            timer.TimerValueFb.LinkInputSig(trilist.StringInput[joinMap.TimerValue]);
-
-            trilist.SetSigTrueAction(joinMap.TimerStart, () =>
-                {
-                    timer.Timer.Start();
-                });
-
-            trilist.SetSigTrueAction(joinMap.TimerCancel, () =>
-                {
-                    timer.Timer.Cancel();
-                });
-
-            trilist.SetSigTrueAction(joinMap.TimerFinish, () =>
-                {
-                    timer.Timer.Finish();
-                });
-
-            trilist.SetUShortSigAction(joinMap.TimerCountdownSet, value =>
-                {
-                    timer.SecondsToCount = value;
-                });
-
-            trilist.SetSigTrueAction(joinMap.TimerExtend, timer.Extend);
-        }
-    }
-
-    public class CountdownTimerJoinMap : JoinMapBase
-    {
-        public uint TimerStart { get; private set; }
-        public uint TimerCancel { get; private set; }
-        public uint TimerFinish { get; private set; }
-        public uint TimerExtend { get; private set; }
-
-        public uint TimerCounting { get; private set; }
-        public uint TimerExpired { get; private set; }
-        public uint TimerWarning { get; private set; }
-
-        public uint TimerPercentage { get; private set; }
-        public uint TimerCountdownSet { get; private set; }
-
-        public uint TimerValue { get; private set; }
-
-
-        CountdownTimerJoinMap()
-        {
-            TimerStart = 1;
-            TimerCancel = 2;
-            TimerFinish = 3;
-            TimerExtend = 4;
-
-            TimerCounting = 1;
-            TimerExpired = 2;
-            TimerWarning = 3;
-
-            TimerCountdownSet = 1;
-
-            TimerPercentage = 1;
-
-            TimerValue = 1;
         }
 
-        public CountdownTimerJoinMap(uint joinStart)
-            : this()
-        {
-            OffsetJoinNumbers(joinStart);
-        }
 
-        public override void OffsetJoinNumbers(uint joinStart)
-        {
-            var joinActual = joinStart - 1;
-
-            GetType()
-                .GetCType()
-                .GetProperties()
-                .Where(prop => prop.PropertyType == typeof(uint).GetCType())
-                .ToList()
-                .ForEach(x => x.SetValue(this, (uint)x.GetValue(this, null) + joinActual, null));
-        }
     }
 }
 
